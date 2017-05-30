4e967de1bb7b59d73dbcc7d7f574a60d7517b98b
Payam Najafizadeh <payamnajafizadeh@Payams-MacBook-Air.local>
2017-05-30 11:49:21 +0100

    fix issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django_categorytree',
-    version='0.1',
+    version='0.1.2',
     description='A django app with an abstract model which helps \
 to define tree style categories with unlimited sub levels',
     url='https://github.com/payamnj/django-categorytree',
